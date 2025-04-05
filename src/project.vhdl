dd389d0d03c8dbe3d16f1616724885ad4a478535
Hhulke <hamgr699@student.liu.se>
2025-04-05 11:48:09 +0200

    V3

--- src/project.vhdl
+++ src/project.vhdl
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-entity Julke_Gussinatorn is
+entity tt_um_Julke_Gussinatorn is
     port (
         ui_in   : in  std_logic_vector(7 downto 0);
         uo_out  : out std_logic_vector(7 downto 0);
@@ -13,9 +13,9 @@ entity Julke_Gussinatorn is
         clk     : in  std_logic;
         rst_n   : in  std_logic
     );
-end Julke_Gussinatorn;
+end tt_um_Julke_Gussinatorn;
 
-architecture Roxen of Julke_Gussinatorn is
+architecture Roxen of tt_um_Julke_Gussinatorn is
 
     type rom_type is array(7 downto 0) of std_logic_vector(7 downto 0);
     signal ROM_Mrse : rom_type := (
@@ -38,15 +38,11 @@ architecture Roxen of Julke_Gussinatorn is
     signal I_ROM_Mrse : integer range 0 to 63 := 0;
 
     begin
-
         --uo_out <= std_logic_vector(unsigned(ui_in) + unsigned(uio_in));
         uo_out <= not (ui_in and uio_in);
         uio_out <= "00000000";
         uio_oe <= "00000000";
 
-        process(clk, rst_n) begin
-            #RESET
-        end process;
 
         process(ui_in(2 downto 0), clk) begin
 
@@ -60,7 +56,7 @@ architecture Roxen of Julke_Gussinatorn is
 
                 if (ui_in_sync(0) = "1") or (ui_in_sync(1) = "1") and not (FLUSH_SR_Mrse = "1") then
                     CELL_Mrse <= ui_in_sync(1 downto 0);
-                else if (FLUSH_SR_Mrse = "1") then
+                elsif (FLUSH_SR_Mrse = "1") then
                     CELL_Mrse <= "00";
                 end if;
 
@@ -68,7 +64,7 @@ architecture Roxen of Julke_Gussinatorn is
                     I_SR_Mrse <= 0;
                     FLUSH_SR_Mrse <= "0";
 
-                else if (FLUSH_SR_Mrse = "1")  or (ui_in_sync(0) = "1") or (ui_in_sync(1) = "1") then
+                elsif (FLUSH_SR_Mrse = "1")  or (ui_in_sync(0) = "1") or (ui_in_sync(1) = "1") then
                         SR_Mrse <= SR_Mrse(SR_Mrse'high - 2 downto SR_Mrse'low) & CELL_Mrse;
                         I_SR_Mrse <= I_SR_Mrse + 1;
                 end if;     
